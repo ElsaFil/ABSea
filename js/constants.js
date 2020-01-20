db2d39983ea352e0b09d6ea9957493fd9b8fe447
Elsa Filippidou <elsa.filippidou@gmail.com>
2020-01-20 16:19:53 +0100

    main game done

--- js/constants.js
+++ js/constants.js
@@ -3,3 +3,54 @@ const CANVAS_HEIGHT = 500;
 const SUN_START_X = 0;
 const CLOUD_START_X = 0;
 const START_Y = 0;
+const allowedChars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+const words_movies = [
+  "Almodovar",
+  "Lobster",
+  "Casablanca",
+  "director",
+  "stuntman",
+  "Rainman"
+];
+const words_nature = [
+  "tulip",
+  "waterfall",
+  "rainbow",
+  "lemur",
+  "beetroot",
+  "lightning"
+];
+const words_general = [
+  "suspenseful",
+  "bootcamp",
+  "development",
+  "convenient",
+  "whiteboard"
+];
--- js/game.js
+++ js/game.js
@@ -1,5 +1,114 @@
-class Game {}
+// class Game {}
 
-class Sun {}
+// class Sun {}
 
-class Cloud {}
+// class Cloud {}
+
+const gameOptions = document.getElementsByClassName("game-options")[0];
+const game = document.getElementsByClassName("game")[0];
+const currentWordDiv = document.getElementsByClassName("current-word")[0];
+
+let category = "";
+let currentWord = "";
+let correctGuesses = [];
+let wrongGuesses = [];
+
+let gameOver = false;
+
+function startGame(e) {
+  let form = e.target.parentElement;
+  let select = form.querySelector("select");
+  let selectedCategory = select.options[select.selectedIndex].value;
+  category = selectedCategory;
+
+  // hide options
+  gameOptions.className += " hidden";
+  // show game
+  let gameClasses = game.className.split(" ").filter(name => {
+    return name !== "hidden";
+  });
+  game.className = gameClasses;
+  // start detecting keyboard input
+  document.onkeypress = guessLetter;
+  // add dark background to copyright
+  let copyright = document.querySelector(".copyright div");
+  copyright.className += "dark-background";
+
+  selectRandomWord();
+  displayCurrentWord();
+}
+
+function selectRandomWord() {
+  let wordPool = [];
+  switch (category) {
+    case "Movies":
+      wordPool = words_movies;
+      break;
+    case "Nature":
+      wordPool = words_nature;
+      break;
+    case "All":
+    default:
+      // all
+      wordPool = words_movies.concat(words_nature).concat(words_general);
+      break;
+  }
+
+  let randomIndex = Math.floor(Math.random() * wordPool.length);
+  currentWord = wordPool[randomIndex];
+  console.log("currentWord: " + currentWord);
+}
+
+function guessLetter(e) {
+  let letter = e.key.toLowerCase();
+  if (!allowedChars.includes(letter)) {
+    console.log("unsupported input: " + letter);
+  }
+  if (
+    currentWord.includes(letter) ||
+    currentWord.includes(letter.toUpperCase())
+  ) {
+    correctGuesses.push(letter);
+  } else {
+    wrongGuesses.push(letter);
+  }
+
+  displayCurrentWord();
+  displayWrongGuesses();
+
+  if (!currentWordDiv.innerText.includes("_")) {
+    alert("You won! 🎉");
+    // stop detecting keyboard input
+    document.onkeypress = null;
+  } else if (wrongGuesses.length === 5) {
+    alert("You lost! ☹️");
+    gameOver = true;
+    // stop detecting keyboard input
+    document.onkeypress = null;
+  }
+}
+
+function displayCurrentWord() {
+  currentWordDiv.innerText = "";
+  for (let i = 0; i < currentWord.length; i++) {
+    let letter = currentWord[i];
+    if (
+      correctGuesses.includes(letter) ||
+      correctGuesses.includes(letter.toLowerCase())
+    ) {
+      currentWordDiv.innerText += " " + letter;
+    } else {
+      currentWordDiv.innerText += " _";
+    }
+  }
+}
+
+function displayWrongGuesses() {
+  let container = document.getElementsByClassName("wrong-guesses-letters")[0];
+  container.innerText = "";
+  wrongGuesses.forEach(letter => {
+    container.innerText += letter + " ";
+  });
+}
+
+document.getElementById("start-button").onclick = startGame;
